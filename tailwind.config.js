a5296ea72dc32e1559e81fc33e2c64ed747d1281
irisrezende <irishanielle16@gmail.com>
2025-10-29 12:16:03 -0300

    Adicionei Tailwind CSS configuração
    Adicionei Tailwind CSS configuração

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  // Habilita o modo escuro baseado em uma classe no <html>
+  darkMode: 'class', 
+  content: [
+    "./*.html", // Diz ao Tailwind para olhar seus arquivos HTML
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
